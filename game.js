4db3bc91f450f66dae00d05219a9d75345b2139f
Liam's Apps <liams.systems@gmail.com>
2025-01-20 08:05:57 -0500

    Add function to play a single round

--- game.js
+++ game.js
@@ -21,7 +21,54 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// console.log(getComputerChoice());
+// console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
 
+	let message = "";
+
+	if (humanChoice === "Rock" && computerChoice === "Scissors") {
+		//console.log("Rock beats Scissors, human wins!");
+		message = "Rock beats Scissors, human wins!";
+		humanScore += 1;
+	}
+	else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+		//console.log("Scissors beats Paper, computer wins!");
+		message = "Scissors beats Paper, computer wins!";
+		computerScore += 1;
+	}
+	else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+		//console.log("Scissors beats Paper, human wins!");
+		message = "Scissors beats Paper, human wins!";
+		humanScore += 1;
+	}
+	else if (humanChoice === "Rock" && computerChoice === "Paper") {
+		//console.log("Paper beats Rock, computer wins!");
+		message = "Paper beats Rock, computer wins!";
+		computerScore += 1;
+	}
+	else if (humanChoice === "Paper" && computerChoice === "Rock") {
+		//console.log("Paper beats Rock, human wins!");
+		message = "Paper beats Rock, human wins!";
+		humanScore += 1;
+	}
+	else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+		//console.log("Rock beats Scissors, computer wins!");
+		message = "Rock beats Scissors, computer wins!";
+		computerScore += 1;
+	}
+	else {
+		console.log("TIE, go again");	
+	}
+
+	//return winner ? humanScore : computerScore;
+	return message;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log("human score: " + humanScore);
+console.log("computer score: " + computerScore);
 
