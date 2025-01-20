2c44f55fda1b578b274dd33becdc68cfd2bce2bd
Liam's Apps <liams.systems@gmail.com>
2025-01-20 06:31:34 -0500

    Add function to get computer choice

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+// .CREATE variables named humanScore and computerScore in the global scope
+// .INIT variables named humanScore and computerScore with the value of 0
+let humanScore = 0;
+let computerScore = 0;
+
+function random () {
+    let options = "Rock,Paper,Scissors".split(',');
+    // let result = options[options.indexOf(Math.floor(Math.random() * 3))];
+    let result = options[Math.floor(Math.random() * 3)];
+    return result;
+}
+
+// .GET computer choice (function)
+function getComputerChoice() {
+    // console.log(random());
+    return random();
+}
+
+console.log(getComputerChoice());
+
+
