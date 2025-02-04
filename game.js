cc086a87cf3b36ec864fc7543436e266f6760fe9
Liam's Apps <liams.systems@gmail.com>
2025-02-04 08:41:13 -0500

    Change event handler to display results and disable buttons

--- game.js
+++ game.js
@@ -9,31 +9,6 @@
 // required by node.js to use prompt in console - NO WORK !!!
 // const prompt = require(‘prompt-sync’)();
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-
-// // WORKS
-// rock.addEventListener("click", () => alert("ROCK"));
-// paper.addEventListener("click", () => alert("PAPER"));
-// scissors.addEventListener("click", () => alert("SCISSORS"));
-
-// WORKS
-rock.addEventListener("click", (e) => {
-	// alert("ROCK", e.target);
-	alert(e.target.id);
-	console.log(e.target.id);
-});
-paper.addEventListener("click", (e) => {
-	alert(e.target.id);
-	console.log(e.target.id);
-});
-scissors.addEventListener("click", (e) => {
-	alert(e.target.id);
-	console.log(e.target.id);
-});
-
-
 // used to return an option for the computer
 function random () {
     let options = "Rock,Paper,Scissors".split(',');
@@ -45,83 +20,97 @@ function getComputerChoice() {
     return random();
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, Paper or Scissors");
-	choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
-	while (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors") {
-		choice = prompt("YOU MUST CHOOSE: Rock, Paper or Scissors");
-		choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+function playRound(humanChoice, computerChoice) {
+
+	let message = "";
+
+	if (humanChoice === "Rock" && computerChoice === "Scissors") {
+		message = "Rock beats Scissors, human wins!";
+		humanScore += 1;
 	}
-    
-    return choice;
+	else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+		message = "Scissors beats Paper, computer wins!";
+		computerScore += 1;
+	}
+	else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+		message = "Scissors beats Paper, human wins!";
+		humanScore += 1;
+	}
+	else if (humanChoice === "Rock" && computerChoice === "Paper") {
+		message = "Paper beats Rock, computer wins!";
+		computerScore += 1;
+	}
+	else if (humanChoice === "Paper" && computerChoice === "Rock") {
+		message = "Paper beats Rock, human wins!";
+		humanScore += 1;
+	}
+	else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+		message = "Rock beats Scissors, computer wins!";
+		computerScore += 1;
+	}
+	else {
+		message = "TIE, go again";	
+	}
+	
+	// return message;
+	return `Round: ${round} ${message}`;
+	// console recognizes \n, but not HTML
+	// return `Round: ${round} ${message}\ntesting\ntesting`;
+	// neither console nor HTML recognizes <br>
+	// return `Round: ${round} ${message}<br>testing<br>testing`;
 }
 
 function playGame() {
-
-	let humanScore = 0;
-	let computerScore = 0;
-	
-	let round = 0;
-	
-	while (humanScore !== 3 && computerScore !== 3) {
-		let humanSelection = getHumanChoice();
-		let computerSelection = getComputerChoice();
-
-		while (humanSelection === computerSelection) {
-			humanSelection = getHumanChoice();
-			computerSelection = getComputerChoice();
-		}
-		console.log(`human: ${humanSelection}`);
-		console.log(`computer: ${computerSelection}`);
-
-		let message = playRound(humanSelection, computerSelection);
-
-		if (message !== "TIE, go again") {
-			round += 1;
-			console.log(`Round #${round}: ${message}`);
-		} 
+	// if (humanScore !== 3 && computerScore !== 3) {
+	if (humanScore === 3 || computerScore === 3) {
+		return (humanScore > computerScore) ? "HUMAN WINS!" : "COMPUTER WINS!";
 	}
-
-	function playRound(humanChoice, computerChoice) {
-
-		let message = "";
-
-		if (humanChoice === "Rock" && computerChoice === "Scissors") {
-			message = "Rock beats Scissors, human wins!";
-			humanScore += 1;
-		}
-		else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-			message = "Scissors beats Paper, computer wins!";
-			computerScore += 1;
-		}
-		else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-			message = "Scissors beats Paper, human wins!";
-			humanScore += 1;
-		}
-		else if (humanChoice === "Rock" && computerChoice === "Paper") {
-			message = "Paper beats Rock, computer wins!";
-			computerScore += 1;
-		}
-		else if (humanChoice === "Paper" && computerChoice === "Rock") {
-			message = "Paper beats Rock, human wins!";
-			humanScore += 1;
-		}
-		else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-			message = "Rock beats Scissors, computer wins!";
-			computerScore += 1;
-		}
-		else {
-			message = "TIE, go again";	
-		}
-		
-		return message;
-	}
-
-	console.log("human score: " + humanScore);
-	console.log("computer score: " + computerScore);
-	return (humanScore > computerScore) ? "HUMAN WINS!" : "COMPUTER WINS!";
 }
 
-// console.log("PLAYING ROCK PAPER SCISSORS");
-// console.log(playGame());
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+
+console.log("human score: " + humanScore);
+console.log("computer score: " + computerScore);
+
+const div = document.querySelector("#results");
+const rock = document.querySelector("#Rock");
+const paper = document.querySelector("#Paper");
+const scissors = document.querySelector("#Scissors");
+
+// function eventHandler(gameMove) {
+function eventHandler() {
+	let result = playRound(this.id, getComputerChoice());
+	console.log(result);
+	let para = document.createElement("p");
+	para.textContent = result;
+	let text1 = document.createTextNode(`	human score: ${humanScore}	`);
+	let text2 = document.createTextNode(`	computer score: ${computerScore}`);
+	para.appendChild(text1);
+	para.appendChild(text2);
+	div.appendChild(para);
+	let winner = playGame();
+	if (winner === "HUMAN WINS!" || winner === "COMPUTER WINS!") {
+		let buttons = document.querySelectorAll("button");
+		buttons.forEach((button) => {
+			button.removeEventListener("click", eventHandler);
+			button.disabled = true;
+		});
+		let para2 = document.createElement("p");
+		para2.textContent = winner;
+		div.appendChild(para2);
+	}
+	round +=1 ;
+}
+
+rock.addEventListener("click", eventHandler);
+paper.addEventListener("click", eventHandler);
+scissors.addEventListener("click", eventHandler);
+
+// NO WORK
+// rock.addEventListener("click", eventHandler("Rock"));
+// paper.addEventListener("click", eventHandler("Paper"));
+// scissors.addEventListener("click", eventHandler("Scissors"));
+
 
